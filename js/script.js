709e4cfb274774ced20dd189341ada5b12278e89
Jabari <leonardo.rodrigues199506@yahoo.com.br>
2021-08-01 13:19:34 -0300

    função para abrir e fechar descrição importada

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 import menuMobile from "./animacoes/menu_mobile.js";
+import mostrarDescricao from "./animacoes/mostrar_descricao.js";
 
 menuMobile(); /*Abrir e fechar menu mobile*/
+mostrarDescricao(); /*Abrir e fechar descricao*/
