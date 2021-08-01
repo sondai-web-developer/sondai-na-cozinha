755b30c9345c140ddb5ee57a91fd87fd37ef83e6
Jabari <leonardo.rodrigues199506@yahoo.com.br>
2021-08-01 17:41:18 -0300

    função mostrar descrição otimizada

--- js/animacoes/mostrar_descricao.js
+++ js/animacoes/mostrar_descricao.js
@@ -1,28 +1,16 @@
 export default function mostrarDescricao() {
-  const iconeOrganico = document.querySelector('[data-descricao="icone_organico"');
-  const iconeReceitas = document.querySelector('[data-descricao="icone_receitas"');
-  const iconeCursos = document.querySelector('[data-descricao="icone_cursos"');
+  const listaIconesDescricao = document.querySelectorAll("[data-descricao=icone]");
+  const listaTextosDescricao = document.querySelectorAll("[data-descricao=texto]");
 
-  const textoOrganico = document.querySelector('[data-descricao="texto_organico"');
-  const textoReceitas = document.querySelector('[data-descricao="texto_receitas"');
-  const textoCursos = document.querySelector('[data-descricao="texto_cursos"');
-
-  function abrirFecharDescricaoOrganico() {
-    iconeOrganico.classList.toggle('mudarIcone');
-    textoOrganico.classList.toggle('mostrarTexto');
+  function mostrarTexto(index) {
+    listaIconesDescricao[index].classList.toggle('mudarIcone');
+    listaTextosDescricao[index].classList.toggle('mostrarTexto');
   }
 
-  function abrirFecharDescricaoReceitas() {
-    iconeReceitas.classList.toggle('mudarIcone');
-    textoReceitas.classList.toggle('mostrarTexto');
-  }
+  listaIconesDescricao.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      mostrarTexto(index);
+    });
+  });
 
-  function abrirFecharDescricaoCursos() {
-    iconeCursos.classList.toggle('mudarIcone');
-    textoCursos.classList.toggle('mostrarTexto');
-  }
-
-  iconeOrganico.addEventListener('click', abrirFecharDescricaoOrganico);
-  iconeReceitas.addEventListener('click', abrirFecharDescricaoReceitas);
-  iconeCursos.addEventListener('click', abrirFecharDescricaoCursos);
 }
