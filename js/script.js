0ee6e5fe04077a55799c49b51f1f305a3fd86478
Jabari <leonardo.rodrigues199506@yahoo.com.br>
2021-08-04 17:31:09 -0300

    slide removido

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 import menuMobile from "./animacoes/menu_mobile.js";
 import mostrarDescricao from "./animacoes/mostrar_descricao.js";
-import Slide from "./animacoes/slide.js";
 
 menuMobile(); /*Abrir e fechar menu mobile*/
 mostrarDescricao(); /*Abrir e fechar descricao*/
-const slide = new Slide('[data-slide="lista"]', '[data-slide="wrapper"]');
-slide.init();
