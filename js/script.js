7cd990471d84f089d0f166d412fc79ddb457ee98
Jabari <leonardo.rodrigues199506@yahoo.com.br>
2021-08-04 18:34:20 -0300

    função logoScrollTop importada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import menuMobile from "./animacoes/menu_mobile.js";
 import mostrarDescricao from "./animacoes/mostrar_descricao.js";
+import logoScrollTop from "./animacoes/logo-scroll-top.js";
 
 menuMobile(); /*Abrir e fechar menu mobile*/
 mostrarDescricao(); /*Abrir e fechar descricao*/
+logoScrollTop(); /*Ao clicar na logo, o scroll vertical irá deslizar até o topo do site*/
